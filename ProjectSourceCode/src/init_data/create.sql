d0bd0ef32398f49806adab3f2e77e1ad1c2e719c
Avery Wagner <avery.wagner@colorado.edu>
2024-03-21 12:45:44 -0600

    create user table

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -1,5 +1,10 @@
 --TODO: Table for users
 
+CREATE TABLE IF NOT EXISTS users (
+    username VARCHAR(50) PRIMARY KEY,
+    password CHAR(60) NOT NULL
+);
+
 --TODO: Users to groups maybe
 
 --TODO: Table for groups
