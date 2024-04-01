5204195738d5744329e941e30a4d8d977d4f8abf
masoniis <bott.m@comcast.net>
2024-04-01 11:55:35 -0600

    database changes and fixed foreign key relation

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -1,35 +1,29 @@
 CREATE TABLE IF NOT EXISTS users (
-		id SERIAL PRIMARY KEY,
-		username VARCHAR(255) NOT NULL,
+		username VARCHAR(255) PRIMARY KEY,
 		password VARCHAR(255) NOT NULL
 );
--- DROP TABLE IF EXISTS users;
--- CREATE TABLE users (
---   username VARCHAR(50) PRIMARY KEY,
---   password CHAR(60) NOT NULL
--- );
 
--- CREATE TABLE IF NOT EXISTS groups (
--- 		id SERIAL PRIMARY KEY,
--- 		name VARCHAR(255) NOT NULL,
--- 		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
--- );
---
--- CREATE TABLE IF NOT EXISTS user_to_groups (
--- 	foreign_key user_id REFERENCES users(id),
--- 	foreign_key group_id REFERENCES groups(id)
--- );
---
--- CREATE TABLE IF NOT EXISTS user_to_transactions (
--- 	foreign_key user_id REFERENCES users(id),
--- 	foreign_key transaction_id REFERENCES transactions(id)
--- );
---
--- CREATE TABLE IF NOT EXISTS transactions (
--- 		id SERIAL PRIMARY KEY,
--- 		amount DECIMAL(10, 2) NOT NULL,
--- 		description TEXT NOT NULL,
--- 		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
--- );
---
--- --TODO: Potentially add views to simplify queries
+CREATE TABLE IF NOT EXISTS groups (
+		name VARCHAR(255) PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS transactions (
+		id SERIAL PRIMARY KEY,
+		amount DECIMAL(10, 2) NOT NULL,
+		description TEXT NOT NULL,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS user_to_groups (
+	username VARCHAR(255) REFERENCES users,
+	groupname VARCHAR(255) REFERENCES groups,
+	PRIMARY KEY (username, groupname)
+);
+
+CREATE TABLE IF NOT EXISTS user_to_transactions (
+	username VARCHAR(255) REFERENCES users,
+	transaction_id INT REFERENCES transactions,
+	PRIMARY KEY (username, transaction_id)
+);
+
+--TODO: Potentially add views to simplify queries
--- ProjectSourceCode/src/init_data/insert.sql
+++ ProjectSourceCode/src/init_data/insert.sql
@@ -1,4 +1,7 @@
-
 INSERT INTO users (username, password) VALUES ('admin', 'admin');
 
-SELECT * FROM users;
+INSERT INTO groups (name) VALUES ('admin group');
+
+-- INSERT INTO users_to_groups (username, groupname) VALUES ('admin', 'admin group');
+
+-- SELECT * FROM users;
