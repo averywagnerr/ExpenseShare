c204ee101bba33e2c40cb78164172e89d751ca1d
masoniis <bott.m@comcast.net>
2024-04-24 08:32:03 -0600

    fixed logout issue!?

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -93,11 +93,11 @@ app.get("/db", (_, res) => {
 		});
 });
 
-app.get("/welcome", (req, res) => {
+app.get("/welcome", (_, res) => {
 	res.json({ status: "success", message: "Welcome!" });
 });
 
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
 	res.render("pages/landing");
 });
 
@@ -285,119 +285,6 @@ app.get("/home", (req, res) => {
 	}
 });
 
-app.post("/deposit", async (req, res) => {
-
-	try {
-
-
-		console.log(req.body)
-
-		let deposit = parseFloat(req.body.deposit_amount)
-		let withdraw = parseFloat(req.body.withdraw_amount)
-
-		let curr_balance = parseFloat(req.session.user.balance)
-
-		let newBalance = curr_balance + (deposit - withdraw)
-
-		newBalance = parseFloat(newBalance)
-
-		const query = await db.none(`UPDATE users SET balance = ${newBalance} WHERE username = '${req.session.user.username}'`)//update user balance in database
-
-		req.session.user.balance = newBalance
-
-		const transactions = db.manyOrNone(
-			// "SELECT * FROM transactions t JOIN user_to_transactions ut ON t.id = ut.transaction_id WHERE ut.username = $1",
-			"SELECT * FROM transactions",
-			req.session.user.id
-		).then((transactions) => {
-			const reciept_transactions = db.manyOrNone(
-				// "SELECT * FROM transactions t JOIN user_to_transactions ut ON t.id = ut.transaction_id WHERE ut.username = $1",
-				"SELECT * FROM reciept_transactions",
-				req.session.user.id
-			).then((reciept_transactions) => {
-				res.render("pages/home", {
-					user: req.session.user,
-					username: req.session.user.username,
-					transactions: transactions,
-					reciept_transactions: reciept_transactions,
-					balance: newBalance,
-				});
-			});
-		});
-
-	} catch (err) {
-
-		console.log(err)
-		res.status(400).send()
-
-	}
-
-})
-
-app.post("/groupexpense", async function(req, res) {
-	//Get the group token
-	// let groupToken = "";
-	// await db.oneOrNone("SELECT * FROM groups WHERE groupname = $1", req.body.groupname).then((group) => {
-	// 	if (!group) {
-	// 		res.render("pages/home", { message: "Group does not exist!", error: true });
-	// 		return;
-	// 	}
-	// 	groupToken = group.grouptoken;
-	// });
-
-
-	//INFO: Make sure the sender is in the group
-	let inGroup = false;
-	await db.oneOrNone("SELECT * FROM user_to_groups WHERE username = $1 AND groupname = $2",
-		[req.session.user.username, req.body.groupname])
-		.then((user) => {
-			if (user) {
-				inGroup = true;
-			}
-		})
-		.catch((err) => {
-			console.error(err);
-			res.render("pages/home", { message: "An error occurred while validating group membership.", error: true });
-			return;
-		});
-
-	if (!inGroup) {
-		res.render("pages/home", { message: "You are not in the group or this group does not exist!", error: true });
-		return;
-	}
-
-	let members = [];
-	await db.manyOrNone("SELECT username FROM user_to_groups WHERE groupname = $1", req.body.groupname)
-		.then((users) => {
-			users.forEach((user) => {
-				console.log("User: ", user);
-				members.push(user.username);
-			});
-		})
-		.catch((err) => {
-			console.error(err);
-			res.render("pages/home", { message: "An error occurred while fetching group members.", error: true });
-			return;
-		});
-
-	console.log("GROUP MEMBERS: ", members);
-
-	for (let i = 0; i < members.length; i++) {
-		if (members[i] !== req.session.user.username) {
-			await db.one("INSERT INTO transactions (sender, receiver, amount, description) VALUES ($1, $2, $3, $4) RETURNING id",
-				[req.session.user.username, members[i], (req.body.expenseamount / members.length), req.body.description]).then((data) => {
-					db.none("INSERT INTO user_to_transactions (username, transaction_id, is_sender) VALUES ($1, $2, $3)", [members[i], data.id, false])
-				})
-				.catch((err) => {
-					console.error(err);
-					res.render("pages/home", { message: "An error occurred while adding group expense.", error: true });
-					return;
-				});
-		}
-	}
-
-	res.render("pages/home", { message: "Successfully added group expense." });
-});
 app.use(transactionRoutes, auth);
 
 app.get("/logout", (req, res) => {
--- ProjectSourceCode/src/routes/transactions.js
+++ ProjectSourceCode/src/routes/transactions.js
@@ -7,135 +7,114 @@ const Router = express.Router();
 
 
 Router.post("/deposit", async (req, res) => {
-  try {
-    console.log(req.body);
+	try {
+		console.log(req.body);
 
-    let deposit = parseFloat(req.body.deposit_amount);
-    let withdraw = parseFloat(req.body.withdraw_amount);
+		let deposit = parseFloat(req.body.deposit_amount);
+		let withdraw = parseFloat(req.body.withdraw_amount);
 
-    let curr_balance = parseFloat(req.session.user.balance);
+		let curr_balance = parseFloat(req.session.user.balance);
 
-    let newBalance = curr_balance + (deposit - withdraw);
+		let newBalance = curr_balance + (deposit - withdraw);
 
-    newBalance = parseFloat(newBalance);
+		newBalance = parseFloat(newBalance);
 
-    const query = await db.none(
-      `UPDATE users SET balance = ${newBalance} WHERE username = '${req.session.user.username}'`
-    ); //update user balance in database
+		const query = await db.none(
+			`UPDATE users SET balance = ${newBalance} WHERE username = '${req.session.user.username}'`
+		); //update user balance in database
 
-    req.session.user.balance = newBalance;
+		req.session.user.balance = newBalance;
 
-    const transactions = db
-      .manyOrNone(
-        // "SELECT * FROM transactions t JOIN user_to_transactions ut ON t.id = ut.transaction_id WHERE ut.username = $1",
-        "SELECT * FROM transactions",
-        req.session.user.id
-      )
-      .then((transactions) => {
-        const reciept_transactions = db
-          .manyOrNone(
-            // "SELECT * FROM transactions t JOIN user_to_transactions ut ON t.id = ut.transaction_id WHERE ut.username = $1",
-            "SELECT * FROM reciept_transactions",
-            req.session.user.id
-          )
-          .then((reciept_transactions) => {
-            res.render("pages/home", {
-              user: req.session.user,
-              username: req.session.user.username,
-              transactions: transactions,
-              reciept_transactions: reciept_transactions,
-              balance: newBalance,
-            });
-          });
-      });
-  } catch (err) {
-    console.log(err);
-    res.status(400).send();
-  }
+		res.redirect("/home");
+
+	} catch (err) {
+		console.log(err);
+		res.status(400).send();
+	}
 });
 
-Router.post("/groupexpense", async function (req, res) {
-  //INFO: Make sure the sender is in the group
-  let inGroup = false;
-  await db
-    .oneOrNone(
-      "SELECT * FROM user_to_groups WHERE username = $1 AND groupname = $2",
-      [req.session.user.username, req.body.groupname]
-    )
-    .then((user) => {
-      if (user) {
-        inGroup = true;
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.render("pages/home", {
-        message: "An error occurred while validating group membership.",
-        error: true,
-      });
-      return;
-    });
+Router.post("/groupexpense", async function(req, res) {
+	//INFO: Make sure the sender is in the group
+	let inGroup = false;
+	await db
+		.oneOrNone(
+			"SELECT * FROM user_to_groups WHERE username = $1 AND groupname = $2",
+			[req.session.user.username, req.body.groupname]
+		)
+		.then((user) => {
+			if (user) {
+				inGroup = true;
+			}
+		})
+		.catch((err) => {
+			console.error(err);
+			res.render("pages/home", {
+				message: "An error occurred while validating group membership.",
+				error: true,
+			});
+			return;
+		});
 
-  if (!inGroup) {
-    res.render("pages/home", {
-      message: "You are not in the group or this group does not exist!",
-      error: true,
-    });
-    return;
-  }
+	if (!inGroup) {
+		res.render("pages/home", {
+			message: "You are not in the group or this group does not exist!",
+			error: true,
+		});
+		return;
+	}
 
-  let members = [];
-  await db
-    .manyOrNone(
-      "SELECT username FROM user_to_groups WHERE groupname = $1",
-      req.body.groupname
-    )
-    .then((users) => {
-      users.forEach((user) => {
-        console.log("User: ", user);
-        members.push(user.username);
-      });
-    })
-    .catch((err) => {
-      console.error(err);
-      res.render("pages/home", {
-        message: "An error occurred while fetching group members.",
-        error: true,
-      });
-      return;
-    });
+	let members = [];
+	await db
+		.manyOrNone(
+			"SELECT username FROM user_to_groups WHERE groupname = $1",
+			req.body.groupname
+		)
+		.then((users) => {
+			users.forEach((user) => {
+				console.log("User: ", user);
+				members.push(user.username);
+			});
+		})
+		.catch((err) => {
+			console.error(err);
+			res.render("pages/home", {
+				message: "An error occurred while fetching group members.",
+				error: true,
+			});
+			return;
+		});
 
-  for (let i = 0; i < members.length; i++) {
-    if (members[i] !== req.session.user.username) {
-      await db
-        .one(
-          "INSERT INTO transactions (sender, receiver, amount, description) VALUES ($1, $2, $3, $4) RETURNING id",
-          [
-            req.session.user.username,
-            members[i],
-            req.body.expenseamount / members.length,
-            req.body.description,
-          ]
-        )
-        .then((data) => {
-          console.log("Transaction data: ", data);
-          db.none(
-            "INSERT INTO user_to_transactions (username, transaction_id, is_sender) VALUES ($1, $2, $3)",
-            [members[i], data.id, false]
-          );
-        })
-        .catch((err) => {
-          console.error(err);
-          res.render("pages/home", {
-            message: "An error occurred while adding group expense.",
-            error: true,
-          });
-          return;
-        });
-    }
-  }
+	for (let i = 0; i < members.length; i++) {
+		if (members[i] !== req.session.user.username) {
+			await db
+				.one(
+					"INSERT INTO transactions (sender, receiver, amount, description) VALUES ($1, $2, $3, $4) RETURNING id",
+					[
+						req.session.user.username,
+						members[i],
+						req.body.expenseamount / members.length,
+						req.body.description,
+					]
+				)
+				.then((data) => {
+					console.log("Transaction data: ", data);
+					db.none(
+						"INSERT INTO user_to_transactions (username, transaction_id, is_sender) VALUES ($1, $2, $3)",
+						[members[i], data.id, false]
+					);
+				})
+				.catch((err) => {
+					console.error(err);
+					res.render("pages/home", {
+						message: "An error occurred while adding group expense.",
+						error: true,
+					});
+					return;
+				});
+		}
+	}
 
-  res.render("pages/home", { message: "Successfully added group expense." });
+	res.redirect("/home");
 });
 
 module.exports = Router;
